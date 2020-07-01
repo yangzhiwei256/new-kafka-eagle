e0d6cc8f2b7ab18e90c9241e7a813de8550e6b3d
zhiwei_yang <yangzhiwei1488@foxmail.com>
2020-07-01 19:12:53 +0800

    优化kafka集群模拟启动脚本

--- src/main/resources/script/start-kafka-cluster.sh
+++ src/main/resources/script/start-kafka-cluster.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # kafka cluster bootstrap script
 # acquire Old kafka Cluster Pids
+# Notices: when script execute error, please open it whit 'vim -b filename', avoid special characters
+# replace special character: sed -i 's/^M//g' start-kafka-cluster.sh
 pids=`jps | grep Kafka | cut -d ' ' -f 1`
 for pid in $pids;
 do
@@ -15,7 +17,17 @@ done
 echo 'begin to start kafka node'
 # kafka Cluster Config Dir
 KAFKA_HOME=/data/apps/kafka_2.13-2.5.0
-KAFKA_CLUSTER_DIR=$KAFKA_HOME/cluster/tmp
+KAFKA_CLUSTER_DIR=/tmp/kafka/cluster
+ZOOKEEPER_SERVER=172.25.87.200:2181,172.25.87.200:2182,172.25.87.200:2183
+
+# kafka node numbers
+KAFKA_NODE_NUM=3
+
+## kafka bootstrap port
+KAFKA_NODE_PORT_PREFIX=909
+
+## use for kafka metric
+KAFKA_JMX_PORT_PREFIX=998
 SERVER_PROPERTIES=server.properties
 
 # kafka Cluster bootstrap single node
@@ -36,34 +48,26 @@ function bootstrapSingleNode() {
     cp "${KAFKA_HOME}/config/${SERVER_PROPERTIES}" ./
     echo 'delete.topic.enable=true' >> $SERVER_PROPERTIES
     echo 'auto.create.topics.enable=false' >> $SERVER_PROPERTIES
-    echo "port=909$index" >> $SERVER_PROPERTIES
+    echo "port=${KAFKA_NODE_PORT_PREFIX}${index}" >> $SERVER_PROPERTIES
     echo "host.name=`hostname`" >> $SERVER_PROPERTIES
 
     ## kafka node args replace
     ## broker.id/port/host.name/log.dirs/zookeeper.connect
-    sed -i "s/broker.id=0/broker.id=$index/g" $SERVER_PROPERTIES
-    sed -i "s/log.dirs=/$KAFKA_NODE_LOG_PATH/g" $SERVER_PROPERTIES
-    export JMX_PORT=998$i
+    sed -i "s|broker.id=.*|broker.id=${index}|g" $SERVER_PROPERTIES
+    sed -i "s|log.dirs=.*|log.dirs=${KAFKA_NODE_LOG_PATH}|g" $SERVER_PROPERTIES
+    sed -i "s|zookeeper.connect=.*|zookeeper.connect=${ZOOKEEPER_SERVER}|g" $SERVER_PROPERTIES
+
+    echo ${KAFKA_JMX_PORT_PREFIX}${index}
+    export JMX_PORT=${KAFKA_JMX_PORT_PREFIX}${index}
 
     ## bootstrap single node
-    nohup  kafka-server-start.sh "${KAFKA_NODE_BASE_PATH}/${SERVER_PROPERTIES}" > /dev/null &
+    echo "command exec args：${KAFKA_NODE_BASE_PATH}/${SERVER_PROPERTIES}, JMX_PORT: ${JMX_PORT}"
+    nohup kafka-server-start.sh "${KAFKA_NODE_BASE_PATH}/${SERVER_PROPERTIES}" > /dev/null &
     echo "===> finish bootstrap kafka ${index} node"
 }
 
-KAFKA_NODE_NUM=3
 for ((i=1; i<=$KAFKA_NODE_NUM; i++))
 do
 	bootstrapSingleNode $i
 done
-
-
-# Enable JMX Function For Metrics
-#export JMX_PORT=9988
-#nohup  kafka-server-start.sh  $KAFKA_CLUSTER_DIR/node1/server.properties > /dev/null &
-
-#export JMX_PORT=9989
-#nohup  kafka-server-start.sh  $KAFKA_CLUSTER_DIR/node2/server.properties > /dev/null &
-
-#export JMX_PORT=9990
-#nohup  kafka-server-start.sh  $KAFKA_CLUSTER_DIR/node3/server.properties > /dev/null &
 
